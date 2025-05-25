e47b97aab5e43853f5bd891b9e75bb2254f39675
Umang Agarwal <umangagarwal257@gmail.com>
2025-05-25 18:56:17 +0530

    Add Transaction struct and make Vouch fields public

--- anchor/programs/veri-fund/src/state/vouch.rs
+++ anchor/programs/veri-fund/src/state/vouch.rs
@@ -3,8 +3,8 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(InitSpace)]
 pub struct Vouch {
-    campaign: Pubkey,
-    voucher: Pubkey,
-    stake_amount: u64,
-    timestamp: i64,
+    pub campaign: Pubkey,
+    pub voucher: Pubkey,
+    pub stake_amount: u64,
+    pub timestamp: i64,
 }
